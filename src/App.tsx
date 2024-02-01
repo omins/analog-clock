551d2882c6c3b28586cd77543ae734a1e1b4c06f
Kevin Minseok Oh <oms3749@gmail.com>
2024-02-01 23:32:41 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className='w-full font-bold'>Analog clock example</div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{tsx,ts}', './index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
