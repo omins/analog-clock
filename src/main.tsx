1397a5d9305a1a4ecfdfa305e93ced9bc1c6f6a5
Kevin Minseok Oh <oms3749@gmail.com>
2024-02-05 23:35:30 +0900

    add: ClockRoot to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import ClockRoot from "./components/Clock/clock-root.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ClockRoot>
+      <App />
+    </ClockRoot>
+  </React.StrictMode>,
 );
