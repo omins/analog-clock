6147e673caba6fc9b1aa5cdfe00a92af7f62c4ab
Kevin Minseok Oh <oms3749@gmail.com>
2024-02-05 12:25:58 +0900

    feat: clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Clock, ClockRoot } from "./components/Clock";
+
 function App() {
   return (
     <>
-      <div className='w-full font-bold'>Analog clock example</div>
+      <div className="mx-auto max-w-3xl bg-white text-black antialiased dark:bg-black">
+        <div className="mx-auto h-[250px] w-[250px]">
+          <ClockRoot>
+            <Clock />
+          </ClockRoot>
+        </div>
+      </div>
     </>
   );
 }
