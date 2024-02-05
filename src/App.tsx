ce0907549f0ddfcf68d1a120c1ba652023e46ae8
Kevin Minseok Oh <oms3749@gmail.com>
2024-02-05 23:39:30 +0900

    feat: clock with tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { Clock, ClockRoot } from "./components/Clock";
+import { Clock, useTimeString } from "./components/Clock";
+import { Tooltip } from "./components/Tooltip";
 
 function App() {
+  const timeString = useTimeString();
+
   return (
     <>
-      <div className="mx-auto max-w-3xl bg-white text-black antialiased dark:bg-black">
-        <div className="mx-auto h-[250px] w-[250px]">
-          <ClockRoot>
-            <Clock />
-          </ClockRoot>
+      <div className="mx-auto flex max-w-3xl justify-center gap-3 bg-white pt-[100px] text-black antialiased dark:bg-black">
+        <div className="flex flex-col items-center justify-center gap-2">
+          <div className="mx-auto flex h-[250px] w-[250px]">
+            <Tooltip placement="right-end" content={timeString} followCursor>
+              <Clock />
+            </Tooltip>
+          </div>
+          <h2>Clock with Tooltip</h2>
         </div>
       </div>
     </>
