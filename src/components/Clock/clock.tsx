5558303dc42b2b9f9b22947ac6aa1a8c8a8bcae5
Kevin Minseok Oh <oms3749@gmail.com>
2024-02-06 03:03:18 +0900

    docs: add clock component's description

--- src/components/Clock/clock.tsx
+++ src/components/Clock/clock.tsx
@@ -4,6 +4,18 @@ import ClockHand from "./hand";
 
 type ClockProps = unknown & React.HTMLAttributes<HTMLDivElement>;
 
+/**
+ * To use clock, you need to wrap it with parent that width and height are defined.
+ *
+ * And make sure to wrap with `<ClockRoot>` at the top level of the app to provide time value.
+ * @example
+ * <ClockRoot>
+ *  ... other components
+ *  <div className="w-32 h-32">
+ *    <Clock />
+ *  </div>
+ * </ClockRoot>
+ */
 const Clock = memo(
   forwardRef<HTMLDivElement, ClockProps>((props, ref) => {
     return (
