1a3ed23b3af7b1fa29f6657360ce1b4a351f13f7
Kevin Minseok Oh <oms3749@gmail.com>
2024-02-06 03:03:44 +0900

    chore: edit demo

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,18 @@ function App() {
 
   return (
     <>
-      <div className="mx-auto flex max-w-3xl justify-center gap-3 bg-white pt-[100px] text-black antialiased dark:bg-black">
-        <div className="flex flex-col items-center justify-center gap-2">
-          <div className="mx-auto flex h-[250px] w-[250px]">
+      <div className="mx-auto flex max-w-3xl justify-center gap-3 bg-white p-10 text-black antialiased dark:bg-black">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <div className="h-40 w-40">
             <Tooltip placement="right-end" content={timeString} followCursor>
               <Clock />
             </Tooltip>
           </div>
           <h2>Clock with Tooltip</h2>
+          <div className="h-40 w-40">
+            <Clock />
+          </div>
+          <h2>Clock without Tooltip</h2>
         </div>
       </div>
     </>
