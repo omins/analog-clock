bc4e0d2d58f65c1d9711474e37d89f33787838b8
Kevin Minseok Oh <oms3749@gmail.com>
2024-02-05 12:21:12 +0900

    create: ClockFace component

---
+++ src/components/Clock/face.tsx
@@ -0,0 +1,49 @@
+import { MarkVariantProps, markVariant } from "./variants";
+
+function ClockFace() {
+  return (
+    <>
+      <CenterMark />
+      <MinuteMark />
+    </>
+  );
+}
+
+function CenterMark() {
+  return (
+    <div className="absolute bottom-1/2 left-1/2 right-1/2 top-1/2 z-10 h-2 w-2 -translate-x-2/4 -translate-y-2/4 rounded-full bg-black" />
+  );
+}
+
+function MinuteMark() {
+  return (
+    <div>
+      {Array.from({ length: 60 }).map((_, idx) => {
+        return (
+          <Mark
+            key={`clock_mark_${idx}`}
+            angle={idx * 6}
+            variant={idx % 5 === 0 ? "five" : "normal"}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+type MarkProps = {
+  angle: number;
+} & MarkVariantProps;
+
+function Mark({ angle = 0, variant }: MarkProps) {
+  return (
+    <div
+      className="absolute bottom-0 left-1/2 right-1/2 top-0"
+      style={{ transform: `rotate(${angle}deg)` }}
+    >
+      <div className={markVariant({ variant })} />
+    </div>
+  );
+}
+
+export default ClockFace;
